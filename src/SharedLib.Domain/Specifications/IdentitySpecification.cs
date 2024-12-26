be709590dcb1ee5ceef8b42e5c543971e4f29907
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2024-12-26 13:38:24 -0300

    fix

--- src/SharedLib.Domain/Specifications/IdentitySpecification.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Linq.Expressions;
-
-namespace SharedLib.Domain.Specifications
-{
-    internal sealed class IdentitySpecification<T> : Specification<T>
-    {
-        public override Expression<Func<T, bool>> ToExpression() => x => true;
-    }
-}
