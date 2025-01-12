6a0d7b1f9f59ed743e4be3f11f26848bb904e181
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-12 19:00:10 -0300

    feat: extension para ler e validar o token

---
+++ src/SharedLib.Tokens/Extensions/JwkList.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace SharedLib.Tokens.Extensions;
+
+public sealed class JwkList(JsonWebKeySet jwkTaskResult)
+{
+    public DateTime When { get; set; } = DateTime.Now;
+    public JsonWebKeySet Jwks { get; set; } = jwkTaskResult;
+}
---
+++ src/SharedLib.Tokens/Extensions/JwksExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Protocols.OpenIdConnect;
+using Microsoft.IdentityModel.Protocols;
+
+namespace SharedLib.Tokens.Extensions;
+
+public static class JwksExtension
+{
+    public static void SetJwksOptions(this JwtBearerOptions options, JwkOptions jwkOptions)
+    {
+        var httpClient = new HttpClient(options.BackchannelHttpHandler ?? new HttpClientHandler())
+        {
+            Timeout = options.BackchannelTimeout,
+            MaxResponseContentBufferSize = 1024 * 1024 * 10 
+        };
+
+        options.ConfigurationManager = new ConfigurationManager<OpenIdConnectConfiguration>(
+            jwkOptions.JwksUri,
+            new JwksRetriever(),
+            new HttpDocumentRetriever(httpClient) { RequireHttps = options.RequireHttpsMetadata });
+
+        options.TokenValidationParameters.ValidateAudience = false;
+        options.TokenValidationParameters.ValidIssuer = jwkOptions.Issuer;
+
+        if (!string.IsNullOrEmpty(jwkOptions.Audience))
+        {
+            options.TokenValidationParameters.ValidateAudience = true;
+            options.TokenValidationParameters.ValidAudience = jwkOptions.Audience;
+        }
+    }
+}
