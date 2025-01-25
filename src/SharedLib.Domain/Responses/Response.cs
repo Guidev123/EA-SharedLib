2587787fcaf99931874423ce92a92d27f3ae62f4
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-25 03:14:25 -0300

    refactor: ajustando response

--- src/SharedLib.Domain/Responses/Response.cs
+++ src/SharedLib.Domain/Responses/Response.cs
@@ -1,17 +1,21 @@
-﻿namespace SharedLib.Domain.Responses
+﻿using System.Text.Json.Serialization;
+
+namespace SharedLib.Domain.Responses
 {
     public class Response<TData>(
+        bool isSucess,
+        int code,
         TData? data,
-        int code = Response<TData>.DEFAULT_STATUS_CODE,
         string? message = null,
-        string[]? errors = null)
+        string[]? errors = null
+        )
     {
         public const int DEFAULT_STATUS_CODE = 200;
-        public int Code { get; } = code;
         public TData? Data { get; set; } = data;
         public string? Message { get; } = message;
         public string[]? Errors { get; } = errors;
-        public bool IsSuccess
-            => Code is >= DEFAULT_STATUS_CODE and <= 299;
+        public bool IsSuccess { get; set; } = isSucess;
+        [JsonIgnore]
+        public int Code { get; set; } = code;
     }
 }
