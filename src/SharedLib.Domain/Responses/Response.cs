ae1560a8ff2463698d65c1cc60466ec835022fba
Guilherme Rafael <155389912+Guidev123@users.noreply.github.com>
2025-01-25 14:52:25 -0300

    refactor: ajustando response

--- src/SharedLib.Domain/Responses/Response.cs
+++ src/SharedLib.Domain/Responses/Response.cs
@@ -3,18 +3,18 @@
 namespace SharedLib.Domain.Responses
 {
     public class Response<TData>(
-        bool isSucess,
-        int code,
         TData? data,
+        int? code = null,
         string? message = null,
-        string[]? errors = null
-        )
+        string[]? errors = null)
     {
+        [JsonIgnore]
+        public readonly int statusCode = code ?? DEFAULT_STATUS_CODE;
+        private const int DEFAULT_STATUS_CODE = 200;
         public TData? Data { get; set; } = data;
         public string? Message { get; } = message;
         public string[]? Errors { get; } = errors;
-        public bool IsSuccess { get; set; } = isSucess;
-        [JsonIgnore]
-        public int Code { get; set; } = code;
+        public bool IsSuccess =>
+            statusCode is >= DEFAULT_STATUS_CODE and <= 299;
     }
 }
